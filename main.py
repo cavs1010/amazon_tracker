72f0433d88d8790f1ee836c834202e58915428e8
Camilo <cavs1010@gmail.com>
2023-11-29 11:26:48 +1000

    Google credentials have been uploaded

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import json
+import os
+from google.oauth2.service_account import Credentials
 import requests
 from bs4 import BeautifulSoup
+
+#TODO Create google sheets
+#TODO Create google sheets connection
 
 # Retrieving the website
 #URL = "https://www.amazon.com.au/dp/B09R51WRV4"
@@ -11,5 +17,9 @@ if response.status_code != 200:
 # Fetch the data using beautiful soup
 soup =BeautifulSoup(response.text, 'html.parser')
 price_product = float(soup.find(name="span", class_="a-offscreen").getText().replace("$",""))
-print(price_product)
+
+
+# Load credentials from the environmental variable
+creds_json = json.loads(os.environ['GOOGLE_APPLICATION_CREDENTIALS_JSON'])
+creds = Credentials.from_service_account_info(creds_json)
 
