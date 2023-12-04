83c1e70d495de484437563fe05fca4d688b99615
Camilo <cavs1010@gmail.com>
2023-12-04 14:36:22 +1000

    I have split the method of price, so I can create the soup just once withinthe class.

--- web_scraping.py
+++ web_scraping.py
@@ -9,6 +9,25 @@ class AmazonProductScraper:
                url (str): URL of the Amazon product page to scrape.
         """
         self.url_product = url
+        self.soup = self.get_website_and_parse_it()
+
+    def get_website_and_parse_it(self):
+        # User-Agent string for Google Chrome
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+
+        # Perform a GET request with the specified headers
+        try:
+            response = requests.get(self.url_product, headers=headers)
+            # Raises an HTTPError for unsuccessful status codes
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise Exception(f"Request failed: {e}")
+
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
 
     def fetch_product_price(self):
         """Fetch and return the product price from the given URL.
@@ -23,31 +42,16 @@ class AmazonProductScraper:
             Exception: If the request fails, the price element is not found, or price conversion fails.
         """
 
-        # User-Agent string for Google Chrome
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-        }
 
-        # Perform a GET request with the specified headers
-        try:
-            response = requests.get(self.url_product, headers=headers)
-            # Raises an HTTPError for unsuccessful status codes
-            response.raise_for_status()
-        except requests.RequestException as e:
-            raise Exception(f"Request failed: {e}")
-
-
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.text, 'html.parser')
 
         # Find the price element using its HTML class
-        price_whole = soup.find(name="span", class_="a-price-whole").getText()
-        price_fraction = soup.find(name="span", class_="a-price-fraction").getText()
+        price_whole = self.soup.find(name="span", class_="a-price-whole").getText()
+        price_fraction = self.soup.find(name="span", class_="a-price-fraction").getText()
         if price_whole and price_fraction:
             try:
                 # Extract the text from the price element and convert it to a float
                 full_price = price_whole+price_fraction
-                name_element = soup.find(name="span", id="productTitle")
+                name_element = self.soup.find(name="span", id="productTitle")
                 print(name_element)
                 return float(full_price)
             except ValueError:
